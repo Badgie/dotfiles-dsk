3834ad5cdc0f7a95da62eb380455b9b1e730e8c3
Badgy <jonaskhansen@gmail.com>
2020-04-10 14:08:04 +0200

    add spicetify theme

--- setup.sh
+++ setup.sh
@@ -12,13 +12,14 @@ sys_symlink() {
 }
 
 # lock screen
+# dependency: betterlockscreen
 symlink "betterlockscreen/betterlockscreenrc" ".config/betterlockscreenrc"
-
 mkdir -p "$HOME/scripts/lock"
 symlink "betterlockscreen/lock.py" "scripts/lock/lock.py"
 symlink "betterlockscreen/lines" "scripts/lock/lines"
 
 # i3 blocks
+# dependency: i3blocks
 mkdir -p "$HOME/.config/i3blocks"
 symlink "i3blocks/mem.py" ".config/i3blocks/mem.py"
 symlink "i3blocks/spotify.py" ".config/i3blocks/spotify.py"
@@ -27,18 +28,24 @@ symlink "i3blocks/cpu.py" ".config/i3blocks/cpu.py"
 symlink "i3blocks/load.py" ".config/i3blocks/load.py"
 symlink "i3blocks/wttr.py" ".config/i3blocks/wttr.py"
 symlink "i3blocks/disk.py" ".config/i3blocks/disk.py"
-
 mkdir -p "$HOME/.config/i3blocks/dmi-weather"
 symlink "i3blocks/dmi-weather/cities" ".config/i3blocks/dmi-weather/cities"
 symlink "i3blocks/dmi-weather/scrloc.py" ".config/i3blocks/dmi-weather/scrloc.py"
 symlink "i3blocks/dmi-weather/weather.py" ".config/i3blocks/dmi-weather/weather.py"
-
 symlink "i3blocks/config" ".config/i3blocks/config"
 symlink "i3blocks/config-small" ".config/i3blocks/config-small"
 
 # i3
+# dependency: i3wm
 mkdir -p "$HOME/.config/i3"
 symlink "i3/config" ".config/i3/config"
+
+# spicetify
+# dependency: spicetify-cli
+mkdir -p "$HOME/.config/spicetify/Themes/Custom"
+mkdir -p "/usr/share/spicetify-cli/Themes/Custom"
+symlink "spicetify/Custom/color.ini" "/usr/share/spicetify-cli/Themes/Custom/color.ini"
+symlink "spicetify/Custom/user.css" "/usr/share/spicetify-cli/Themes/Custom/user.css"
 
 # shufflewall
 sys_symlink "shufflewall/shufflewall.service" "/etc/systemd/system/shufflewall.service"
